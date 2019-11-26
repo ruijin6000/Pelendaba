6562af7bace99c4f0fd2c077fff832b1b1b30676
RuiJin <rui.jin@sjsu.edu>
2019-11-25 23:04:17 -0800

    log out added

--- services/passport.js
+++ services/passport.js
@@ -4,13 +4,40 @@ const mongoose = require('mongoose');
 const keys= require('../config/keys');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null,user.id);
+
+
+});
+
+passport.deserializeUser((id,done)=> {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken,refreshToken,profile,done)=>{
-        new User({googleId:profile.id}).save();
+        User.findOne({googleId: profile.id})
+            .then((existingUser)=>{
+                if(existingUser) {
+                    // we already have a record with the given profile ID
+                    done(null,existingUser);
+                } else {
+                    // we don't have a user record with this ID. make a new record
+                    new User({googleId:profile.id})
+                        .save()
+                    .then(user => done(null,user));
+                }
+            });
+
 
         console.log('access token',accessToken);
         console.log('refreshToken',refreshToken);
