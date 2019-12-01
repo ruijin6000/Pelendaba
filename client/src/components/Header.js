f4c82d2e098953e4d8574e9396a414bf3cf43779
RuiJin <rui.jin@sjsu.edu>
2019-12-01 00:22:12 -0800

    fixed the redirect issue that not go back to /auth/google/callback  (pasport.use styling)

---
+++ client/src/components/Header.js
@@ -0,0 +1,42 @@
+import React , {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null :
+                return 'Still deciding';
+            case false :
+                return <li><a href="/auth/google">Login With Google </a></li>;
+            default :
+                return <li><a href="/api/logout">LogOut</a></li>;
+        }
+    }
+
+    render(){
+       // console.log(this.props);
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                       { this.renderContent()}
+
+                    </ul>
+                </div>
+            </nav>
+        );
+
+
+
+    }
+
+
+}
+
+function mapStateToProps(state) {
+    return { auth: state.auth};
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
+import {FETCH__USER} from "../actions/types";
 
-export default function(state = {}, action) {
+
+export default function(state = null, action) {
+    console.log(action);
     switch(action.type) {
+        case FETCH__USER :
+             return action.payload || false ;
         default :
             return state;
     }
