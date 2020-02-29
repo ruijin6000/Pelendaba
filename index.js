96a9ad639ceb7bea6b9f7c9f9a97479562d894be
RuiJin <rui.jin@sjsu.edu>
2020-02-29 11:38:20 -0800

    add Survey and Recipient modle

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys')
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 //console.developers.google.com
 
 
