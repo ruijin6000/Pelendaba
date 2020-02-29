f63683b2edf9853aa5b6e99c2c95e3f8039dfe69
RuiJin <rui.jin@sjsu.edu>
2020-02-29 04:47:10 -0800

    more

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,27 +3,27 @@ const passport = require('passport');
 module.exports = app => {
 
     app.get('/auth/google',
-        passport.authenticate('google',{
-            scope: ['profile','email']
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
         })
     );
 
     app.get('/auth/google/callback',
         passport.authenticate('google'),
-        (req , res ) => {
+        (req, res) => {
             res.redirect('/surveys');
         }
     );
 
-    app.get('/api/logout',(req,res)=> {
+    app.get('/api/logout', (req, res) => {
         req.logout();
         res.redirect('/');
         // res.send(req.user);
     });
 
 
-    app.get('/api/current_user',(req,res) => {
-       res.send(req.user);
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
     });
 
 };
