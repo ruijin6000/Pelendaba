9a6ca0d4175f8230b76707f9348d58594253a515
RuiJin <rui.jin@sjsu.edu>
2020-03-02 23:12:45 -0800

    add surveysFomr Field New components

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component  {
+
+   render() {
+       return (
+         <div>
+             <SurveyForm/>
+         </div>
+       );
+
+
+   }
+}
+
+export default SurveyNew;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
-import {combineReducers } from "redux";
+import {combineReducers} from "redux";
+import {reducer as reduxForm} from 'redux-form';
 import authReducer from "./authReducer";
 
-export default combineReducers( {
-    auth   :  authReducer,
+
+export default combineReducers({
+    auth: authReducer,
+    form: reduxForm
 
 });
