766e13a995b5038b684798a9fb61c76652af5f41
RuiJin <rui.jin@sjsu.edu>
2020-03-02 23:49:23 -0800

    add next and cancel buttons

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,12 +2,13 @@ import React, {Component} from "react";
 import {reduxForm, Field} from 'redux-form';
 import SurveyField from './SurveyField';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
 
 const FIELDS = [
     {label: 'Survey Title', name: 'title'},
     {label: 'Subject Line', name: 'subject'},
     {label: 'Email Body', name: 'body'},
-    {label: 'Recipient List', name: 'Emails'}
+    {label: 'Recipient List', name: 'emails'}
 ];
 
 class SurveyForm extends Component {
@@ -17,23 +18,30 @@ class SurveyForm extends Component {
         //     <Field label="Survey Title" type="text" name="title" component={SurveyField}/>
         //     <Field label="Subject Line" type="text" name="subject" component={SurveyField}/>
         //     <Field label="Email Body" type="text" name="body" component={SurveyField}/>
-        //     <Field label="Recipient List" type="text" name="Emails" component={SurveyField}/>
+        //     <Field label="Recipient List" type="text" name="emails" component={SurveyField}/>
         // </div>
 
         return _.map(FIELDS, ({label, name}) => {
             return (
-                <Field component={SurveyField} type="text" label={label} name={name}/>
+                <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
             );
         });
 
     }
 
     render() {
+
         return (
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit"> Submit</button>
+                    <Link to="/surveys"  className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className ="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
