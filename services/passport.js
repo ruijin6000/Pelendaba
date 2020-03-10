46684a878414e568397e331da5608bb9fb757e48
RuiJin <rui.jin@sjsu.edu>
2020-03-10 00:52:24 -0700

    add keys to prod and modify google oauth to asyn -> await

--- services/passport.js
+++ services/passport.js
@@ -20,33 +20,28 @@ passport.deserializeUser((id, done) => {
 
 });
 
+/********************** GOOGLE OAUTH ********************************************/
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-            .then((existingUser) => {
-                if (existingUser) {
-                    // we already have a record with the given profile ID
-                    console.log("Existed User");
-                    done(null, existingUser);
-                } else {
-                    // we don't have a user record with this ID. make a new record
-                    new User({googleId: profile.id})
-                        .save()
-                        .then(user => done(null, user));
-                    console.log("New User");
-                }
-            });
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+        }, async (accessToken, refreshToken, profile, done) =>  {
+            const existingUser = await User.findOne({googleId: profile.id});
+            if (existingUser) {
+                console.log("Existed User", existingUser);
+                done (null, existingUser);
+            } else {
+                const user = await new User({googleId: profile.id}).save();
+                done(null, user);
+                console.log("New User");
+            }
+        }
+    ));
 
-
-    })
-);
-
-
+/********************** FACEBOOK OAUTH ********************************************/
 passport.use(
     new FacebookStrategy({
             clientID: keys.faceBookID,
@@ -76,31 +71,31 @@ passport.use(
     ));
 
 
+
+
+
 // passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: keys.googleClientID,
-//             clientSecret: keys.googleClientSecret,
-//             callbackURL: '/auth/google/callback',
-//             proxy: true
-//         },
-//         async (accessToken, refreshToken, profile, done) => {
-//             const existingUser = await User.findOne({googleId: profile.id});
+//     new GoogleStrategy({
+//         clientID: keys.googleClientID,
+//         clientSecret: keys.googleClientSecret,
+//         callbackURL: '/auth/google/callback',
+//         proxy: true
+//     }, (accessToken, refreshToken, profile, done) => {
+//         User.findOne({googleId: profile.id})
+//             .then((existingUser) => {
+//                 if (existingUser) {
+//                     // we already have a record with the given profile ID
+//                     console.log("Existed User");
+//                     done(null, existingUser);
+//                 } else {
+//                     // we don't have a user record with this ID. make a new record
+//                     new User({googleId: profile.id})
+//                         .save()
+//                         .then(user => done(null, user));
+//                     console.log("New User");
+//                 }
+//             });
 //
-//             if (existingUser) {
 //
-//                 console.log("Existed User", existingUser);
-//                 return (null, existingUser);
-//             } else {
-//
-//                 const user = await new User({googleId: profile.id}).save();
-//                 done(null, user);
-//                 console.log("New User");
-//             }
-//         }
-// ));
-// console.log('access token',accessToken);
-// console.log('refreshToken',refreshToken);
-// console.log('profile',profile);
-// console.log('done',done);
-//     ));
+//     })
+// );
