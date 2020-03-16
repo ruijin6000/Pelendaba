2aad6d84fb61e18f6f804428a2cc8b1a0221a6af
RuiJin <rui.jin@sjsu.edu>
2020-03-15 20:47:37 -0700

    Ip request + GraphQl

--- index.js
+++ index.js
@@ -40,10 +40,49 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+/******  Get Ip Address *********/
+const requestIp = require('request-ip');
+
+// inside middleware handler
+const ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req);
+    next();
+};
+
+app.use(requestIp.mw());
+
+app.use(function(req, res) {
+    const ip = req.clientIp;
+    console.log("ip: "+ ip);
+    res.end(ip);
+});
+
+/******** Graphql *******/
+const graphqlHttp = require('express-graphql');
+const graphqlSchema= require('./graphql/schema');
+const graphqlResolver= require('./graphql/resolvers');
+app.use('/graphql',graphqlHttp({
+    schema : graphqlSchema,
+    rootValue : graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+        if(!err.originalError){
+            return err;
+        }
+        const data= err.originalError.data;
+        const message= err.message || 'An error occurred';
+        const code = err.originalError.code || 500;
+        return {message :message, status : code, data: data};
+
+    }
+}));
+/** ********************/
+
 app.use(express.static('client/build'));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
